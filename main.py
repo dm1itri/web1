87406c7d3780bfe5835c0eccc6c5cee32e93e3ce
dm1itri <dimma-18@yandex.ru>
2022-02-08 18:30:03 +0300

    Пейзажи Марса

--- main.py
+++ main.py
@@ -8,8 +8,24 @@ def name():
     return "Миссия Колонизация Марса"
 
 
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="static/img_planet.png">
+                    <p>Вот она какая, красная планета</p>
+                  </body>
+                </html>"""
+
+
 @app.route('/carousel')
-def index():
+def carousel():
     return """
     <!doctype html>
 <html lang="ru">
