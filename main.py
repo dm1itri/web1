38f33cf4317e78c56d230914b0bc36d05658701f
dm1itri <dimma-18@yandex.ru>
2022-02-08 19:15:11 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def name():
-    return "Миссия Колонизация Марса"
+    return """<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Марс</title>
+                    <link rel="shortcut icon" href="static/head.png" type="image/x-icon">
+                  </head>
+                  <body>
+                    <h1>Миссия Колонизация Марса</h1>
+                  </body>
+                </html>"""
 
 
 @app.route('/image_mars')
@@ -15,11 +25,37 @@ def image_mars():
                   <head>
                     <meta charset="utf-8">
                     <title>Привет, Марс!</title>
+                    <link rel="shortcut icon" href="static/head.png" type="image/x-icon">
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="static/img_planet.png">
                     <p>Вот она какая, красная планета</p>
+                  </body>
+                </html>"""
+
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    d = {'Марс': ['Близок к Земле', 'Много ресурсов', 'Яндекс сказал, что на нем есть вода', 'Красивый'],
+         'Юпитер': ['Увесистый', 'Раньше владел наибольшим количеством спутников в Солнечной системе', 'Защищиает Землю от метеоритов', 'Машины на нем имеют хорошую управляемость'],
+         'Меркурий': ['Самый маленькая', 'Приезжайте сюда, если ты теплолюбивый человек', 'Можно поставить рекорд по прыжкам в высоту', 'Быстро вращается вокруг Солнца'],
+         'Земля': ['Комфортная для жизни', 'Происходит много политических событий', 'Сможешь сделать вдох', 'К сожалению, здесь заставляют работать']}
+    d = d.get(planet_name, ['Наш телескоп ещё не исследовал эту планету' for _ in range(4)])
+    return f"""<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                    <link rel="shortcut icon" href="static/head.png" type="image/x-icon">
+                  </head>
+                  <body>
+                    <h1 align="center">Моё предложение: {planet_name}</h1>
+                    <p class="text-light bg-dark">{d[0]}</p>
+                    <p class="bg-warning">{d[1]}</p>
+                    <p class="bg-success">{d[2]}</p>
+                    <p class="bg-secondary">{d[3]}</p>
                   </body>
                 </html>"""
 
