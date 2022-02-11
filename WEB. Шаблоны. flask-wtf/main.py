508666db5e053e12f0aeb02108725827f0b632db
dm1itri <dimma-18@yandex.ru>
2022-02-11 18:41:43 +0300

    Список профессий

--- WEB. Шаблоны. flask-wtf/main.py
+++ WEB. Шаблоны. flask-wtf/main.py
@@ -13,5 +13,10 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<ol>')
+def list_prof(ol):
+    return render_template('list_prof.html', ol=ol)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
