3e62ce950ab0b2b04c3298be41ff6afd63fa1fa1
dm1itri <dimma-18@yandex.ru>
2022-02-11 18:10:56 +0300

    Тренировки в полёте

--- WEB. Шаблоны. flask-wtf/main.py
+++ WEB. Шаблоны. flask-wtf/main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('index.html', title=title)
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
 
 
 if __name__ == '__main__':
