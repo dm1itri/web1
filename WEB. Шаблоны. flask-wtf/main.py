8184d74c5a6a19f29b33986e5e250119660b44d4
dm1itri <dimma-18@yandex.ru>
2022-02-26 00:46:43 +0300

    Галерея с загрузкой

--- WEB. Шаблоны. flask-wtf/main.py
+++ WEB. Шаблоны. flask-wtf/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -16,6 +16,22 @@ def training(prof):
 @app.route('/list_prof/<ol>')
 def list_prof(ol):
     return render_template('list_prof.html', ol=ol)
+
+
+@app.route('/galery', methods=['GET', 'POST'])
+def galery():
+    if request.method == 'GET':
+        return render_template('galery.html')
+    elif request.method == 'POST':
+        with open('static/image/load_image3.png', 'wb') as f:
+            with open('static/image/load_image2.png', 'rb') as f1:
+                f.write(f1.read())
+        with open('static/image/load_image2.png', 'wb') as f:
+            with open('static/image/load_image1.png', 'rb') as f1:
+                f.write(f1.read())
+        with open('static/image/load_image1.png', 'wb') as f:
+            f.write(request.files['file'].read())
+        return render_template('galery.html')
 
 
 if __name__ == '__main__':
