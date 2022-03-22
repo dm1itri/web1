264c829a391e884e0446b5d42fbddd007e7eeb9d
dm1itri <dimma-18@yandex.ru>
2022-03-22 17:11:59 +0300

    По каютам!

--- WEB. Шаблоны. flask-wtf/main.py
+++ WEB. Шаблоны. flask-wtf/main.py
@@ -57,6 +57,11 @@ def login():
     return render_template('login.html', form=form)
 
 
+@app.route('/distribution')
+def distribution():
+    members = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
+    return render_template('distribution.html', members=members)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
